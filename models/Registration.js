b4e75796f50673837be8a15a4f062a69aa66e236
mahima1911 <mahimamahima18056@it.ssn.edu.in>
2021-04-27 15:43:10 +0530

    second commit

---
+++ models/Registration.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const registrationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+  },
+  phone: {
+    type: Number,
+    trim: true,
+  },
+  branch: {
+    type: String,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Registration", registrationSchema);
